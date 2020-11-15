138bf76a4e7275ef68dbd055b8b0b3c8cad64b92
h6k2 <sepak.hirokazu10@gmail.com>
2020-11-15 19:59:01 +0900

    アニメーション処理

---
+++ main.js
@@ -0,0 +1,11 @@
+// $(window).on('load', function() {
+//     // リストを非表示
+//     $('ul.main_kv li').hide();
+//     // 繰り返し処理
+//     $('ul.main_kv li').each(function(i) {
+//     // 遅延させてフェードイン
+//     $(this).delay(500 * i).fadeIn(2000);
+//     });
+// });
+
+
