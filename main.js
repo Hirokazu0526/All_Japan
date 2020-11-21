69fecb801fae7d75790e3f180b45ffe2e84696f9
h6k2 <sepak.hirokazu10@gmail.com>
2020-11-21 15:49:05 +0900

    Vue追加

--- main.js
+++ main.js
@@ -8,4 +8,23 @@
 //     });
 // });
 
-
+var app = new Vue({
+	el:"#app",
+  data:{
+  	groupA:{
+        team1:"A.S.WAKABA A",
+        team2:"SC TOKYO AXIS",
+        team3:"阪神酒販Dees'TC",
+        team4:"A.K Tipplar",
+        result:["1位","2位","3位","4位"]
+    },
+  	groupB:{
+        team1:"A.S.WAKABA A",
+        team2:"SC TOKYO AXIS",
+        team3:"阪神酒販Dees'TC",
+        team4:"A.K Tipplar",
+        result:["1位","2位","3位","4位"]
+    }
+  }
+  
+})
