d5c009363440aef933e0b156d7e13b8a9534aa0b
h6k2 <sepak.hirokazu10@gmail.com>
2020-11-23 17:42:23 +0900

    vue template追加

--- main.js
+++ main.js
@@ -11,20 +11,64 @@
 var app = new Vue({
 	el:"#app",
   data:{
-  	groupA:{
-        team1:"A.S.WAKABA A",
-        team2:"SC TOKYO AXIS",
-        team3:"阪神酒販Dees'TC",
-        team4:"A.K Tipplar",
-        result:["1位","2位","3位","4位"]
+  	groups:[
+    {
+      id:"1",
+      groupName:"A",
+      name1:" A.S. WAKABA A ",
+      name2:" SC TOKYO AXIS ",
+      name3:" 阪神酒販 Dee's TC ",
+      name4:" A.K tippler ",
+      rsl:"順位",
+      rsl2:"1位",
+      rsl3:"2位",
+      rsl4:"3位",
+      rsl5:"4位",
+      scr1:"２１-１９ ２１-１８",//全角数字で入力
+      scr2:"２１-１６ ２１-１７",
+      scr3:"２１-１５ ２１-１４",
+      scr4:"２１-１２ ２１-１３",
+      scr5:"２１-１１ ２１-１０",
+      scr6:"２１-０９ ２１-０８"
     },
-  	groupB:{
-        team1:"A.S.WAKABA A",
-        team2:"SC TOKYO AXIS",
-        team3:"阪神酒販Dees'TC",
-        team4:"A.K Tipplar",
-        result:["1位","2位","3位","4位"]
+    {
+      id:"2",
+      groupName:"B",
+      name1:" teamE ",
+      name2:" teamF ",
+      name3:" teamG ",
+      name4:" teamH ",
+      rsl:"順位",
+      rsl2:"2位",
+      rsl3:"1位",
+      rsl4:"3位",
+      rsl5:"4位",
+      scr1:"２１-１９ ２１-１８",//全角数字で入力
+      scr2:"２１-１６ ２１-１７",
+      scr3:"２１-１５ ２１-１４",
+      scr4:"２１-１２ ２１-１３",
+      scr5:"２１-１１ ２１-１０",
+      scr6:"２１-０９ ２１-０８"    
+    },
+    {
+      id:"3",
+      groupName:"C",
+      name1:" teamE ",
+      name2:" teamF ",
+      name3:" teamG ",
+      name4:" teamH ",
+      rsl:"順位",
+      rsl2:"2位",
+      rsl3:"1位",
+      rsl4:"3位",
+      rsl5:"4位",
+      scr1:"２１-１９ ２１-１８",//全角数字で入力
+      scr2:"２１-１６ ２１-１７",
+      scr3:"２１-１５ ２１-１４",
+      scr4:"２１-１２ ２１-１３",
+      scr5:"２１-１１ ２１-１０",
+      scr6:"２１-０９ ２１-０８"
     }
+    ]
   }
-  
 })
